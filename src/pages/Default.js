2c7b34db61bae9bc8b448f3e17db3a0643533c97
Jovan R <jocoraos@yahoo.com>
2021-03-31 14:19:41 +0200

    added 404

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 class Default extends Component {
   render() {
     return (
-      <div>
-        <h4>Error page</h4>
-      </div>
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">You Are On Wrong Page</h2>
+        <Link to="/" className="text-uppercase btn btn-danger btn-lg mt-5">
+          go home
+        </Link>
+      </Header>
     );
   }
 }
