0da18018261bb30d1b04fa40c8396cf9db746aa8
Jovan R <jocoraos@yahoo.com>
2021-03-31 13:39:21 +0200

    added button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
-    return <Header />;
+    return (
+      <Header title="Meal Aplication">
+        <Link to="search" className="text-uppercase btn btn-info btn-lg mt-5">
+          search for meals
+        </Link>
+      </Header>
+    );
   }
 }
 
