2b8e16dfa3dba2c84357e5dd0d049c8238dc2d5e
Jovan R <jocoraos@yahoo.com>
2021-04-02 10:43:04 +0200

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
-export default class Home extends Component {
+
+class Home extends Component {
   render() {
     return (
       <Header title="amazing recipes">
         <Link
           to="categories"
-          className="text-uppercase btn btn-info btn-lg mt-3"
-        >
-          search recipes
+          className="text-uppercase btn btn-info btn-lg mt-5">
+          search meals
         </Link>
       </Header>
     );
   }
 }
+export default Home;
