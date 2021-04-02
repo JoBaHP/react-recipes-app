a075ee3f028fdf55b1cd3651c4bc9b7f452481dd
Jovan R <jocoraos@yahoo.com>
2021-04-02 16:30:19 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Categories from "./components/Categories";
 import CategoryPage from "./components/CategoryPage";
 import RecipePage from "./components/RecipePage";
+import NavBar from "./components/NavBar";
 import ContactUs from "./pages/ContactUs";
 import Default from "./pages/Default";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
